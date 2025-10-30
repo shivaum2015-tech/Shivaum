82521b47c54f671670a9a524808cc6f31e9dbf5f
shivaum2015-tech <shivaum2015@gmail.com>
2025-10-30 17:28:01 +0000

    Add files via upload

---
+++ Date_Time.py
@@ -0,0 +1,7 @@
+from datetime import date,time,datetime
+# calling the today
+# function of date class
+today=date.today()
+now = datetime.now()
+print ("Todays date is",today)
+print ("\nCurrent date and time is",now)
