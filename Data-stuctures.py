55fc0b66bbbccc9745da1fdb1dc4b3e1e4d3af7c
shivaum2015-tech <shivaum2015@gmail.com>
2025-11-13 18:41:53 +0000

    Add files via upload

---
+++ Data-stuctures.py
@@ -0,0 +1,10 @@
+subjects=["maths","english","science","history","geography","french","art"]
+print (len(subjects))
+print (subjects[4])
+subjects[5]="spanish"
+print (subjects)
+grade=["A+","C","B+","F-","F+","A","D"]
+result=subjects=grade
+print (result)
+for i in result:
+    print(i)
