e770082e6d6146c408a12440cd43116341754df6
shivaum2015-tech <shivaum2015@gmail.com>
2025-12-11 18:20:18 +0000

    Add files via upload

---
+++ Patterns.py
@@ -0,0 +1,12 @@
+n=5
+for i in range (n):
+    for j in range (i,n-1):
+        print (" ",end ="")
+     
+    for j in range(i):
+        print("🔺", end="")
+
+    for j in range(i+1):
+        print("🔺", end="")
+
+    print()
