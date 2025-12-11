ae12cb97433727a372f1e36abe738f950050f90a
shivaum2015-tech <shivaum2015@gmail.com>
2025-12-11 18:19:16 +0000

    Add files via upload

---
+++ Turtlelibrary.py
@@ -0,0 +1,11 @@
+import turtle
+screen = turtle.Screen()
+screen.bgcolor("lightblue")
+pen = turtle.Turtle()
+pen.color("darkblue")
+pen.pensize(3)
+pen.speed(2)
+for i in range(4):
+ pen.forward(100) 
+ pen.right(90) 
+turtle.done()
