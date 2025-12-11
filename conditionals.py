e9278a76dae68a493ffe9085a3e4323eb22bc5b8
shivaum2015-tech <shivaum2015@gmail.com>
2025-12-11 18:23:14 +0000

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,7 @@
+
+print("this is a theme park wich you have to be ten to ride on any of the rides  ")
+age=(int)(input ("enter your age" ))
+if age >=10:
+   print("You are allowed to go on the ride")
+else:
+   print("You are not allowed to go on the ride")   
