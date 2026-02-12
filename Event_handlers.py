26a7b7be01fc5e8809358819354bc85279b35235
shivaum2015-tech <shivaum2015@gmail.com>
2026-02-12 18:51:40 +0000

    Add files via upload

---
+++ Event_handlers.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.geometry("200x200")
+
+def msg():
+    messagebox.showwarning("Alert", "Stop! virus Found.")
+
+button = Button(root, text="Scan for virus", command=msg)
+button.place(x=40, y=80)
+
+root.mainloop()
